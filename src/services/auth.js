da006722efdd42cc13cca551e09119d1f7ca3dfc
jackson-dos-santos <jackson144@gmail.com>
2021-09-23 12:23:07 -0300

    Delete auth.js

--- src/services/auth.js
+++
@@ -1,4 +0,0 @@
-export const TOKEN_KEY = "@mad:token";
-export const isAuthenticated = () => sessionStorage.getItem(TOKEN_KEY) !== null;
-export const getToken = () => sessionStorage.getItem(TOKEN_KEY);
-console.log(isAuthenticated());
