7b5c102794f6d5a38bcb29813bde9c9bbc2d8899
jackson-dos-santos <jackson144@gmail.com>
2021-09-23 12:24:22 -0300

    Update MenuItem.jsx

--- src/components/AsideLeft/MenuItem.jsx
+++ src/components/AsideLeft/MenuItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-export default function MenuItem({label,route, icon}) {
-  return ( <li><Link to={`/app/${route}`}><span className={`${icon} fa`} /> {label}</Link></li> )
+export default function MenuItem({label,route, icon, disabled=false}) {
+  return ( <li><Link to={disabled === false ? `/app/${route}` : '#'}><span className={`${icon} fa`} />{label} {disabled === true && (<span className="fa fa-lock"/>)}  </Link></li> )
 }
