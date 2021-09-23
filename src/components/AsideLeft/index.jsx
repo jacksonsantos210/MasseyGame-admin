a22e5a427ac8c13c32d12740bd4d8ae39c7d2758
jackson-dos-santos <jackson144@gmail.com>
2021-09-23 12:23:45 -0300

    Update index.jsx

--- src/components/AsideLeft/index.jsx
+++ src/components/AsideLeft/index.jsx
@@ -8,14 +8,14 @@ export default function AsideLeft() {
     <div id="left-menu">
       <div className="sub-left-menu scroll">
         <ul className="nav nav-list">
-          {/* <li><div className="left-bg" /></li> */}
-          <Clock/>
+          <li><div /* className="left-bg" */ style={{ height: 20}} /></li>
+         {/*  <Clock/> */}
           <MenuItem label="Dashboard" route="" icon="fa-home"/>
           <MenuItem label="PLayers" route="players" icon="fa-gamepad"/>
           <MenuItem label="Influencers" route="influencers" icon="fa-users"/>
           <MenuItem label="Tokens" route="influencers-tokens" icon="fa-puzzle-piece"/>
           <MenuItem label="Figurinhas" route="figures" icon="fa-id-card"/>
-          <MenuItem label="Videos" route="videos" icon="fa-video-camera"/>
+          <MenuItem label="Videos" route="videos" icon="fa-video-camera" disabled={true}/>
 
         </ul>
       </div>
