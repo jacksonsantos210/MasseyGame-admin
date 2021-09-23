67295e17bba68cb53288383830bd1bdf1acbffee
jackson-dos-santos <jackson144@gmail.com>
2021-09-23 12:24:06 -0300

    Update index.jsx

--- src/layouts/AuthLayout/index.jsx
+++ src/layouts/AuthLayout/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {LoginWrapper} from './styles';
-import backgroundImage from '@/assets/images/background.png';
+import '@/assets/css/login.css';
+/* import {LoginWrapper} from './styles';
+import backgroundImage from '@/assets/images/background.png'; */
 
 export default function AuthLayout({children}) {
   return (
-    <LoginWrapper background={backgroundImage}>
-      {children}
-    </LoginWrapper>
+    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+      <div class="container">
+        {children}
+      </div>
+    </main>
   )
 }
