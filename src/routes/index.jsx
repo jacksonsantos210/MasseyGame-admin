528b8159852e54afba8bd4c6c770917f7c8c01bb
jackson-dos-santos <jackson144@gmail.com>
2021-09-23 12:24:25 -0300

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,36 +1,27 @@
-/* import React, {useContext} from 'react';
-import AuthContext from '@/contexts/authContext';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import AuthRoutes from './AuthRoutes';
-import AppRoutes from './AppRoutes';
-
-const Routes = () => {
-  const { signed } = useContext(AuthContext);
-  return signed ? <AppRoutes /> : <AuthRoutes />;
-};
-
-export default Routes;
- */
-import Dashboard from '../views/Dashboard/index'
-import Players from '../views/Players/index'
+import Dashboard from '@/views/Dashboard/index'
+import Players from '@/views/Players/index'
 import PlayersShow from '@/views/Players/Show';
-import Influencers from '../views/Influencers/index'
+import Influencers from '@/views/Influencers/index'
 import InfluencersInsert from '@/views/Influencers/Insert';
 import InfluencersEdit from '@/views/Influencers/Edit';
 import InfluencersShow from '@/views/Influencers/Show';
-import InfluencersTokens from '../views/InfluencersTokens/index'
+import InfluencersTokens from '@/views/InfluencersTokens/index'
 import InfluencersTokensInsert from '@/views/InfluencersTokens/Insert';
 import InfluencersTokensEdit from '@/views/InfluencersTokens/Edit';
 import InfluencersTokensShow from '@/views/InfluencersTokens/Show';
-import Figures from '../views/Figures/index'
+import Figures from '@/views/Figures/index'
 import FiguresEdit from '@/views/Figures/Edit';
 import FiguresShow from '@/views/Figures/Show';
-import LogIn from '../views/Auth/LogIn/index'
-import LogOut from '../views/Auth/LogOut/index'
+import LogIn from '@/views/Auth/LogIn/index'
+import LogOut from '@/views/Auth/LogOut/index'
+import Profile from '@/views/Profile/index'
+import Password from '@/views/Password/index'
 
-import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { isAuthenticated } from "@/services/auth";
+
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -51,7 +42,9 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={()=> <Redirect to="/app"/>} />
-      <PrivateRoute exact path="/app/" component={Dashboard} />
+      <PrivateRoute exact path="/app" component={Dashboard} />
+      <PrivateRoute exact path="/app/profile" component={Profile} />
+      <PrivateRoute exact path="/app/password" component={Password} />
       <PrivateRoute exact path="/app/players" component={Players} />
       <PrivateRoute exact path="/app/players/show/:id" component={PlayersShow} />
       <PrivateRoute exact path="/app/influencers" component={Influencers} />
@@ -66,7 +59,7 @@ const Routes = () => (
       <PrivateRoute exact path="/app/figures/edit/:id" component={FiguresEdit} />
       <PrivateRoute exact path="/app/figures/show/:id" component={FiguresShow} />
       <Route exact path="/login" component={LogIn} />
-      <PrivateRoute exact path="/app/sair" component={LogOut} />
+      <PrivateRoute exact path="/app/logout" component={LogOut} />
       <Route path="*" component={() => <Redirect to="/" />} />
     </Switch>
   </BrowserRouter>
